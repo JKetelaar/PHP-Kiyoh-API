dc088d7bf16bb47fa721533037499cf244c6a247
Jeroen Ketelaar <jeroen@ketelaar.me>
2019-11-24 22:36:43 -0600

    [FEATURE] Added tests

--- tests/Unit/KiyohTest.php
+++ tests/Unit/KiyohTest.php
@@ -1,10 +1,30 @@
 <?php
+/**
+ * @author JKetelaar
+ */
 
 namespace JKetelaar\Kiyoh\Tests\Unit;
 
+use JKetelaar\Kiyoh\Kiyoh;
 use PHPUnit\Framework\TestCase;
 
 final class KiyohTest extends TestCase
 {
+    public function testInit()
+    {
+        $kiyohKey = getenv('KIYOH_KEY');
+        $this->assertNotFalse($kiyohKey);
 
+        $kiyoh = new Kiyoh($kiyohKey, 5);
+
+        $company = $kiyoh->getCompany();
+        $this->assertNotNull($company);
+
+        $averageRating = $company->getAverageRating();
+        $this->assertGreaterThan(0, $averageRating);
+
+        $reviews = $kiyoh->getCompany()->getReviews();
+        $this->assertNotNull($reviews);
+        $this->assertEquals(5, count($reviews));
+    }
 }
