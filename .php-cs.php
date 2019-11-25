b3e2647a74d2a4d4512e2f4b5683f604a4656404
Jeroen Ketelaar <jeroen@ketelaar.me>
2019-11-24 23:35:42 -0600

    [FEATURE] Added php-cs-fixer

---
+++ .php-cs.php
@@ -0,0 +1,19 @@
+<?php
+return PhpCsFixer\Config::create()
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        'full_opening_tag' => true,
+        'no_superfluous_phpdoc_tags' => false,
+        'phpdoc_align' => true,
+        'phpdoc_no_access' => true,
+        'phpdoc_no_alias_tag' => true,
+        'yoda_style' => false,
+        'phpdoc_to_return_type' => true,
+        'phpdoc_to_param_type' => true,
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->exclude('vendor')
+        ->in(__DIR__ . '/src')
+    )
+;
