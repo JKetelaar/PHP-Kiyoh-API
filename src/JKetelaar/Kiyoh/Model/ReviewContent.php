465e2946dca619ef9e5025ce58346e439833ae19
Robin Speekenbrink <robin.speekenbrink@synetic.nl>
2022-11-17 17:49:18 +0100

    feat: several PHP8 features:
    - constructor property promotion
    - match instead of switch/case
    - dropped superfluous docblocks

--- src/JKetelaar/Kiyoh/Model/ReviewContent.php
+++ src/JKetelaar/Kiyoh/Model/ReviewContent.php
@@ -11,61 +11,22 @@ namespace JKetelaar\Kiyoh\Model;
 class ReviewContent
 {
     /**
-     * @var string
-     */
-    private $group;
-
-    /**
-     * @var string
-     */
-    private $type;
-
-    /**
-     * @var string
-     */
-    private $rating;
-
-    /**
-     * @var int
-     */
-    private $order;
-
-    /**
-     * @var string
-     */
-    private $translation;
-
-    /**
      * ReviewContent constructor.
-     *
-     * @param string $group
-     * @param string $type
-     * @param string $rating
-     * @param int    $order
-     * @param string $translation
      */
-    public function __construct(string $group, string $type, string $rating, int $order, string $translation)
-    {
-        $this->group = $group;
-        $this->type = $type;
-        $this->rating = $rating;
-        $this->order = $order;
-        $this->translation = $translation;
+    public function __construct(
+        private string $group,
+        private string $type,
+        private string $rating,
+        private int $order,
+        private string $translation
+    ) {
     }
 
-    /**
-     * @return string
-     */
     public function getGroup(): string
     {
         return $this->group;
     }
 
-    /**
-     * @param string $group
-     *
-     * @return ReviewContent
-     */
     public function setGroup(string $group): self
     {
         $this->group = $group;
@@ -73,60 +34,39 @@ class ReviewContent
         return $this;
     }
 
-    /**
-     * @return mixed
-     */
-    public function getRating()
+    public function getRating(): int|bool|string
     {
         $rating = $this->rating;
-        switch ($this->getType()) {
-            case 'INT':
-                $rating = (int) $rating;
-                break;
-            case 'BOOLEAN':
-                $rating = filter_var($rating, FILTER_VALIDATE_BOOLEAN);
-                break;
-            default:
-                $rating = (string) $rating;
-                break;
-        }
-
-        return $rating;
+        return match ($this->getType()) {
+            'INT' => (int) $rating,
+            'BOOLEAN' => filter_var($rating, FILTER_VALIDATE_BOOLEAN),
+            default => (string) $rating,
+        };
     }
 
-    /**
-     * @param mixed $rating
-     *
-     * @return ReviewContent
-     */
-    public function setRating(string $rating): self
-    {
-        switch ($this->getType()) {
-            case 'BOOLEAN':
-                $rating = $this->validateRating($rating);
-                break;
-            default:
-                $rating = (string) $rating;
-                break;
-        }
-        $this->rating = $rating;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
     public function getType(): string
     {
         return $this->type;
     }
 
-    /**
-     * @param string $type
-     *
-     * @return ReviewContent
-     */
+    public function setRating($rating): self
+    {
+        $this->rating = match ($this->getType()) {
+            'BOOLEAN' => $this->validateRating($rating),
+            default => (string) $rating,
+        };
+
+        return $this;
+    }
+
+    private function validateRating($rating): string
+    {
+        if (is_bool($rating)) {
+            return $rating ? 'true' : 'false';
+        }
+        return $rating;
+    }
+
     public function setType(string $type): self
     {
         $this->type = $type;
@@ -134,29 +74,11 @@ class ReviewContent
         return $this;
     }
 
-    /**
-     * @param string $rating
-     *
-     * @return string
-     */
-    private function validateRating(string $rating): string
-    {
-        return $rating;
-    }
-
-    /**
-     * @return int
-     */
     public function getOrder(): int
     {
         return $this->order;
     }
 
-    /**
-     * @param int $order
-     *
-     * @return ReviewContent
-     */
     public function setOrder(int $order): self
     {
         $this->order = $order;
@@ -164,19 +86,11 @@ class ReviewContent
         return $this;
     }
 
-    /**
-     * @return string
-     */
     public function getTranslation(): string
     {
         return $this->translation;
     }
 
-    /**
-     * @param string $translation
-     *
-     * @return ReviewContent
-     */
     public function setTranslation(string $translation): self
     {
         $this->translation = $translation;
